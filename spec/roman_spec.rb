7e592c843b2e6e568230d3497e3d0117889bd0ab
Helena.Franczak <helena.franczak@myob.com>
2019-11-17 15:35:39 +1100

    cleaned up tests to make simpler

--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -1,43 +1,23 @@
 require "number"
 
 RSpec.describe "Roman numerals" do
-  it "represents 1 as I" do
-    expect( Number.to_roman(1) ).to eq "I"
-  end
-
-  it "represents 2 as II" do
-    expect( Number.to_roman(2) ).to eq "II"
-  end
-
-  it "represents 3 as III" do
-    expect( Number.to_roman(3) ).to eq "III"
-  end
-  
-  it "represents 5 as V" do
-    expect( Number.to_roman(5) ).to eq "V"
-  end
-  
-  it "represents 10 as X" do
-    expect( Number.to_roman(10) ).to eq "X"
-  end
-  
-  it "represents 50 as L" do
-    expect( Number.to_roman(50) ).to eq "L"
-  end
-  
-  it "represents 100 as C" do
-    expect( Number.to_roman(100) ).to eq "C"
-  end
-  
-  it "represents 6 as VI" do
-    expect( Number.to_roman(6) ).to eq "VI"
-  end
-    
-  it "represents 11 as XI" do
-    expect( Number.to_roman(11) ).to eq "XI"
-  end
-          
-  xit "represents 12 as XII" do
-    expect( Number.to_roman(12) ).to eq "XII"
+  [
+    {arabic: 1, roman: "I"},
+    {arabic: 2, roman: "II"},
+    {arabic: 3, roman: "III"},
+#     {arabic: 4, roman: "IV"},
+    {arabic: 5, roman: "V"},
+    {arabic: 6, roman: "VI"},
+    {arabic: 10, roman: "X"},
+    {arabic: 11, roman: "XI"},
+#     {arabic: 12, roman: "XII"},
+    {arabic: 50, roman: "L"},
+    {arabic: 100, roman: "C"},
+#     {arabic: 500, roman: "?"},
+#     {arabic: 1000, roman: "M"},
+  ].each do |values|
+    it "represents arabic #{values[:arabic]} as roman #{values[:roman]}" do
+        expect( Number.to_roman(values[:arabic]) ).to eq values[:roman]
+    end
   end
 end
