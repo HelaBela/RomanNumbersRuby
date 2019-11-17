f223b0af6809df58922a6bad24b56be5c79213d6
Helena.Franczak <helena.franczak@myob.com>
2019-11-17 16:04:05 +1100

    if anything left push it on the roman number array
    until there is no remainder

--- lib/number.rb
+++ lib/number.rb
@@ -7,33 +7,19 @@ class Number
     100 => "C"
   }
 
-#     require "pry"
-#     binding.pry
+#       require "pry"; binding.pry
   def self.to_roman(decimal_number)
     roman_number=[]
-
-    last_closest_number = closest_number(decimal_number)
-    reminder = nil
-
-    reminder = decimal_number - last_closest_number 
-  
-    if ROMAN_VALUES.key?(decimal_number)
-        return ROMAN_VALUES[decimal_number]
+    remainder = decimal_number
+    while remainder > 0 do
+      current_decimal_number = closest_number(remainder)
+      roman_number.push ROMAN_VALUES[current_decimal_number]
+      remainder = remainder - current_decimal_number
     end
-    
-    if decimal_number<4
-    decimal_number.times {
-      roman_number.push ROMAN_VALUES[1]
-    }
-    elsif decimal_number>3
-        roman_number.push ROMAN_VALUES[last_closest_number]
-        roman_number.push ROMAN_VALUES[reminder]
-    end
-
     roman_number.join
   end
   
   def self.closest_number(decimal_number)
-    ROMAN_VALUES.keys.reverse.find{|value| value < decimal_number} || 0
+    ROMAN_VALUES.keys.reverse.find{|value| value <= decimal_number} || 1
   end
 end
